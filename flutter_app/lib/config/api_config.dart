c5e594fd30767ce6424bf8510fbfe827eb8f095c
“dakyumlee” <oicrcutie@gmail.com>
2025-11-22 16:37:37 +0900

    Fix API URL for production

--- flutter_app/lib/config/api_config.dart
+++ flutter_app/lib/config/api_config.dart
@@ -1,8 +1,3 @@
 class ApiConfig {
-  static const String localBaseUrl = 'http://localhost:8080/api';
-  
-  static String get baseUrl {
-    const apiUrl = String.fromEnvironment('API_URL', defaultValue: localBaseUrl);
-    return apiUrl;
-  }
+  static const String baseUrl = 'https://stressdebuggerbackend-production.up.railway.app/api';
 }
