d9dc741c2807d1e80fba6627cdf190327df288d4
“dakyumlee” <oicrcutie@gmail.com>
2025-11-23 18:26:53 +0900

    Set timezone to Asia/Seoul

--- spring_api/src/main/java/com/stressdebugger/StressDebuggerApplication.java
+++ spring_api/src/main/java/com/stressdebugger/StressDebuggerApplication.java
@@ -3,8 +3,17 @@ package com.stressdebugger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StressDebuggerApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(StressDebuggerApplication.class, args);
     }
